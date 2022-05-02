e3336d9d62a56aa12e996027afc9851b6095091c
dengyazhou <1283536241@qq.com>
2022-05-02 12:56:51 +0800

    LeetCode 开始

---
+++ LeetCode/LeetCode/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by 邓亚洲 on 2022/5/2.
+//
+
+/**
+ 1、macOS -> Application -> Command Line Tool -> Language 选C++
+ */
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
